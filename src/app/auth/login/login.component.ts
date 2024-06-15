0b7ff10d6edbfa5baf40571bab4f74a2d5137d05
Braulio-Manrique <brauliocuenta12@gmail.com>
2024-06-15 14:15:17 -0500

    angular material, rutas, guard

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
+import { AuthService } from '../auth.service';
+import { Route, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MaterialModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  constructor(private authService: AuthService,
+    private router: Router){
+
+  }
+  submit(usuario:HTMLInputElement,
+    password:HTMLInputElement){
+      this.authService.login(usuario.value,password.value)
+      this.router.navigateByUrl("/dashboard")
+    }
+}
